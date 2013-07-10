e9fb0ed85a06b445cd9cf557419ebdb5fd1d2572
markmichaelrichter <markmichaelrichter@gmail.com>
2013-07-10 01:36:33 -0700

    Added index.html

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  var write = fs.readFileSync('./index.html');
+  var text = write.toString();
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
